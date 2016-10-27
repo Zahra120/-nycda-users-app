a236dc6f9f75f2b000e314f6d0b8f3f95056d0c5
Zahra Ghanbari <ghanbarifam@gmail.com>
2016-10-27 13:37:52 +0200

    done route 4

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var findUser = function(query){
    for (var i=0; i < users.length; i++) {
       if (users[i].firstname.toLowerCase().match(query.toLowerCase())) {
          return users[i];
-      } else if(users[i].lastname.toLowerCase().match(query.toLowerCase()))  {
+      } else if (users[i].lastname.toLowerCase().match(query.toLowerCase()))  {
 
          return users[i];
       }
@@ -41,6 +41,24 @@ app.post('/searchform', function(req, res){
    res.redirect('/search/' + req.body.query);
 });
 
+//route 4: renders a page with three forms on it (first name, last name, and email)
+//that allows you to add new users to the users.json file.
+app.get('/adduser', function(req, res){
+   res.send(pug.renderFile('views/adduser.pug'));
+
+});
+
+app.post('/adduser', function(req, res){
+   console.log('post request on adduser page');
+    users.push({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email
+   });
+   res.redirect('/');
+});
+//route 5: takes in the post request from the 'create user' form, then adds the
+//user to the users.json file. Once that is complete, redirects to the route that displays all your users (from part 0)
 
 
 app.listen(3000, function(){
