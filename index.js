361e46a2e601b198a2d2b44b43cf82e30eda79bd
Zahra Ghanbari <ghanbarifam@gmail.com>
2016-10-27 16:58:06 +0200

    almost finish

--- index.js
+++ index.js
@@ -4,21 +4,37 @@ var pug = require('pug');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var app =  express();
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var users = JSON.parse(fs.readFileSync('users.json'));
 var findUser = function(query){
-   for (var i=0; i < users.length; i++) {
-      if (users[i].firstname.toLowerCase().match(query.toLowerCase())) {
-         return users[i];
-      } else if (users[i].lastname.toLowerCase().match(query.toLowerCase()))  {
-
-         return users[i];
+   var result = [];
+   for (var i=0; i < users.length; i++){
+      if (users[i].firstname.toLowerCase().includes(query.toLowerCase()) || users[i].lastname.toLowerCase().includes(query.toLowerCase())){
+         result.push(users[i]);
       }
    }
-};
 
+   return result;
+};
+// function findUser(input) {
+//   for (i = 0; i < users.length; i++) {
+//     if (searchFirstName(input, users[i]) || searchLastName(input, users[i])) {
+//       return users[i];
+//     }
+//   }
+// }
+//
+// function searchFirstName(input, user) {
+//   return user.firstname.toLowerCase().includes(input.toLowerCase());
+// }
+//
+// function searchLastName(input, user) {
+//   return user.lastname.toLowerCase().includes(input.toLowerCase());
+// }
+//
 // //Create one route: route 1: renders a page that displays all your users.
 app.get('/', function(req,res){
    res.send(pug.renderFile('views/index.pug', { users: users }));
@@ -33,8 +49,8 @@ app.get('/search', function(req, res){
 //route 3: takes in the post request from your form, then displays matching users
 //on a new page. Users should be matched based on whether either their first or last name contains the input string.
 app.get('/search/*', function(req, res){
-   var foundUser = findUser(req.params[0]);
-   res.send(pug.renderFile('views/user.pug', { user: foundUser }));
+   var result = findUser(req.params[0]);
+   res.send(pug.renderFile('views/user.pug', { result: result }));
 });
 app.post('/searchform', function(req, res){
    console.log('post request on search page');
@@ -50,11 +66,14 @@ app.get('/adduser', function(req, res){
 
 app.post('/adduser', function(req, res){
    console.log('post request on adduser page');
-    users.push({
+      users.push({
       firstname: req.body.firstname,
       lastname: req.body.lastname,
       email: req.body.email
    });
+
+   
+   fs.writeFileSync('users.json', JSON.stringify(users));
    res.redirect('/');
 });
 //route 5: takes in the post request from the 'create user' form, then adds the
