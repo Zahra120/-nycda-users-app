5d06d6eababf90a931f84263f1de221f1a8de92f
Zahra Ghanbari <ghanbarifam@gmail.com>
2016-11-06 21:00:35 +0100

    html()

--- public/custom.js
+++ public/custom.js
@@ -3,7 +3,7 @@ $('#searchNav').on('keyup', function() {
    var query = $(this).val();
 
    $.get('/api/search/' + query, function(body){
-
+      $('.search-results').html('');
       body.forEach(function(element) {
        $('.search-results').append(
          '<li>' + element.firstname + ' ' +
